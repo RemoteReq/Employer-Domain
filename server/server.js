2b6097789609eb31ca2580adf263f9963237e82e
ryanferrer <ryferr8122@gmail.com>
2021-05-20 16:44:44 -0500

    move port to 8080

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const history = require('connect-history-api-fallback');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 8083;
+const PORT = 8080;
 
 // User history to intecept client requests and forward to React Router history
 app.use(history());
